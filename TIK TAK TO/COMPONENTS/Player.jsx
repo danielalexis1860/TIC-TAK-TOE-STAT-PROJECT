703d87e4ca10e4c4375dba59d3005e435800c651
danielferguson <107422624+danielalexis1860@users.noreply.github.com>
2024-04-07 20:27:55 -0700

    Add files via upload

---
+++ TIK TAK TO/COMPONENTS/Player.jsx
@@ -0,0 +1,33 @@
+ import { useState }  from 'react';
+
+
+export default function Player({name, symbol}){
+    const [ isEditing, setIsEditing ]  = useState(false);
+
+    function handleEditClick() {
+       // setIsEditing(isEditing ? false : true);  
+       setIsEditing(!isEditing); 
+    }
+
+    let playerName = <span className="player-name">{name}</span>;
+    // let btnCaption = 'Edit';
+
+    if (isEditing)  {
+        playerName = <input  type="text" required value={name}/>;
+      //  btnCaption = 'Save';
+
+    }
+
+    return(
+        <li>
+        <span className="player">
+      {/*    <span className="player-name">{name}</span>          */}
+
+         {playerName}
+        <span className="player-symbol">{symbol}</span>
+        </span>
+        <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+       </li>
+
+    );
+}
